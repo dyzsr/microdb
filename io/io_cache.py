72b959941ae064f0656109b81b419848de611a47
Macaulish64 <justfreefood@163.com>
2019-07-05 18:01:17 +0800

    add io.func
    add io.func

--- io/io_cache.py
+++ io/io_cache.py
@@ -5,3 +5,21 @@
 # 作用：作为缓存区，接受缓存区管理器的管理，读/写数据
 # 主要涉及： 增,删,改,查,整体写入,整体写出
 
+
+class CacheBlock:
+    # 增
+    def insert_table_entry(self):
+        return
+
+    # 删
+    def delete_table_entry(self):
+        return
+
+    # 改
+    def update_table_entry(self):
+        return
+
+    # 查
+    def select_table_entry(self):
+        return
+
--- io/io_cache_manager.py
+++ io/io_cache_manager.py
@@ -2,14 +2,51 @@
 # -*- coding:utf-8 -*-
 # 作者：Macaulish
 # 创建：2019-07-05
-# 作用：作为缓存区管理器，接受上层请求，然后对缓存区进行操作，也负责将缓存区数据写到本地/本地写到缓存区
+# 作用：1. 作为缓存区管理器，接受上层请求(增,删,改,查),
+#      2. 然后对缓存区进行操作
+#       3. 向下传递给存储管理器
+# 关于查询！！！需要自己看看物理计划的执行
 #
-
+# 管理器实例化一些cache块
 # ========================================================
 # import
 # ----1. pickle
 # ----
 
+import io.StoreManager
 
 
 class IoCacheManager:
+    # 立即表载入
+    def load_right_now(self, table_name):
+        return
+
+    # 立即表载出
+    def store_right_now(self):
+        return
+
+    # 增
+    def insert_table_entry(self):
+        return
+
+    # 删
+    def delete_table_entry(self):
+        return
+
+    # 改
+    def update_table_entry(self):
+        return
+
+    # 查
+    def select_table_entry(self):
+        return
+
+    # 创表
+    def create_table(self):
+        return
+
+    # 删表
+    def delete_table(self):
+        return
+
+

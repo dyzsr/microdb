f169e3eb36399b77ef617d979d980526ed0dd294
dongyan <dybest@icloud.com>
2019-07-09 02:22:27 +0800

    update UI & WebServer

--- web/run.py
+++ web/run.py
@@ -2,12 +2,30 @@ import tornado.ioloop
 import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        #self.write('Hi GET')
-        self.render('index.html', title='microdb')
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "*")
+        self.set_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
 
     def post(self):
-        self.write('Hi POST')
+        data = self.get_argument('data', '')
+        print(data)
+
+        result = {
+                'results': ({
+                    'meta': ('a', 'b', 'c'),
+                    'values': (
+                        (1, -2., 'c3'),
+                        (4, 5.5, 'c6'),
+                        )
+                    },)
+                }
+        self.write(result)
+
+    def options(self):
+        # no body
+        self.set_status(204)
+        self.finish()
 
 
 def make_app():
